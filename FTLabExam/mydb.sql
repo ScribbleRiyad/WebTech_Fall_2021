d715945941caf2329916d6efbcb4e2b82568bc15
MD. RIYAD MAHMUD <55358498+Scribble-Riyad@users.noreply.github.com>
2021-12-07 13:10:17 +0600

    Update mydb.sql

--- FTLabExam/mydb.sql
+++ FTLabExam/mydb.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.4
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 07, 2021 at 07:23 AM
--- Server version: 10.4.17-MariaDB
--- PHP Version: 8.0.0
+-- Generation Time: Dec 07, 2021 at 07:34 AM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -40,11 +40,11 @@ CREATE TABLE `employee` (
 --
 
 INSERT INTO `employee` (`ID`, `Name`, `Department`, `JoiningDate`, `Salary`) VALUES
-('E015', 'Alice', 'CSE', '05/05/2020', '50000'),
+('E015', 'Alice', 'IT', '05/05/2020', '50000'),
 ('E021', 'Bob', 'HR', '19/10/2020', '65000'),
 ('E013', 'Charlie', 'Finance', '11/10/2018', '70000'),
 ('E025', 'Danny', 'IT', '19/10/2018', '150000'),
-('E009', 'Erick', 'CSE', '0.000247402276100940', '7500'),
+('E009', 'Erick', 'Finance', '04/07/2017', '65000'),
 ('E005', 'Hobbs', 'IT', 'Nano 19/07/2019', '80000');
 COMMIT;
 
